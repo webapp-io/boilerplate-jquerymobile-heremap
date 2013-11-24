aeb4022f98c6a3dbee78e30297c88ac632677b8c
Philipp Bosch <hello@pb.io>
2013-11-24 19:23:05 +0100

    Add plugin and boilerplate JS code.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
+// Replace with your credentials - see: http://developer.here.com/docs/maps_js/common/credentials.html
 // $.mobile.heremap.app_id = 'your app id';
 // $.mobile.heremap.app_code = 'your app code';
+
+$('#map').on('heremapready', configureMap);
+
+function configureMap(event, map) {
+
+}
